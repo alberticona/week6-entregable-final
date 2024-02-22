d167f5147175652404947266948e5c89058d1ef3
alberticona <albert.ticona@ucsp.edu.pe>
2024-02-22 15:35:41 -0500

    advances

---
+++ src/routes/category.router.js
@@ -0,0 +1,16 @@
+const { getAll, create, remove } = require('../controllers/category.controllers');
+const express = require('express');
+const verifyJWT = require('../utils/verify.JWT');
+
+const routerCategory = express.Router();
+
+routerCategory.route('/')
+    .get(getAll)
+    .post(verifyJWT,create);
+
+routerCategory.route('/:id')
+
+    .delete(remove)
+
+
+module.exports = routerCategory;
