e16fb67a06e61da6aaa66e5e89d6d019457d0061
alberticona <albert.ticona@ucsp.edu.pe>
2024-02-28 07:12:40 -0500

    advances

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const routerUser = require('./user.router');
 const routerCategory = require('./category.router');
 const routerProduct = require('./product.router');
+const routerCart = require('./cart.router');
+const verifyJWT = require('../utils/verify.JWT');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use('/users', routerUser)
 router.use('/categories', routerCategory)
 router.use('/products', routerProduct)
+router.use('/cart', verifyJWT, routerCart) //🔐🔐🔐🔐💪😊
 
 
 module.exports = router;
